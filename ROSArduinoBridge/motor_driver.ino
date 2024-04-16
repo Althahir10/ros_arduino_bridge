cba5f61f27cc9f2dcff6420bde05e941e2ddd92e
Althahir10 <127445963+Althahir10@users.noreply.github.com>
2024-04-16 23:10:48 +0000

    change

--- ROSArduinoBridge/motor_driver.ino
+++ ROSArduinoBridge/motor_driver.ino
@@ -1,24 +1,33 @@
-/***************************************************************
-   Motor driver definitions
-   
-   Add a "#elif defined" block to this file to include support
-   for a particular motor driver.  Then add the appropriate
-   #define near the top of the main ROSArduinoBridge.ino file.
-   
-   *************************************************************/
 
+​
+​
 #ifdef USE_BASE
    
-#ifdef POLOLU_VNH5019 
+#ifdef POLOLU_VNH5019
 #elif defined Spark_Motor_Controller
+
+  void setMotorSpeed(int i, int spd) {
+    if (i == LEFT) RIGHT_MOTOR_PWM.write(spd );
+    else LEFT_MOTOR_PWM.write(spd );  }
+
+  void setMotorSpeeds(int leftSpeed, int rightSpeed) {
+    setMotorSpeed(LEFT, leftSpeed);
+    setMotorSpeed(RIGHT, rightSpeed);                 }
+
+
+
+
+
+
+
 /*  void initMotorController() {
     digitalWrite(RIGHT_MOTOR, HIGH);
     digitalWrite(LEFT_MOTOR, HIGH);
   }
-  */
+ 
   void setMotorSpeed(int i, int spd) {
     unsigned char reverse = 0;
-  
+ 
     if (spd < 0)
     {
       spd = -spd;
@@ -26,21 +35,21 @@
     }
     if (spd > 255)
       spd = 255;
-    
-    if (i == LEFT) { 
-      if      (reverse == 0) { LEFT_MOTOR_PWM.write(spd ); LEFT_MOTOR_PWM.write(spd ); }
-      else if (reverse == 1) { LEFT_MOTOR_PWM.write(spd ); LEFT_MOTOR_PWM.write(spd ); }
+   
+    if (i == LEFT) {
+      if      (reverse == 0) { LEFT_MOTOR_PWM.write(spd ); }
+      else if (reverse == 1) { LEFT_MOTOR_PWM.write(spd ); }
     }
-    else /*if (i == RIGHT) //no need for condition*/ {
-      if      (reverse == 0) { RIGHT_MOTOR_PWM.write(spd ); RIGHT_MOTOR_PWM.write(spd ); }
-      else if (reverse == 1) { RIGHT_MOTOR_PWM.write(spd ); RIGHT_MOTOR_PWM.write(spd ); }
+    else /*if (i == RIGHT) //no need for condition {
+      if      (reverse == 0) { RIGHT_MOTOR_PWM.write(spd );}
+      else if (reverse == 1) { RIGHT_MOTOR_PWM.write(spd );}
     }
   }
-  
+ 
   void setMotorSpeeds(int leftSpeed, int rightSpeed) {
     setMotorSpeed(LEFT, leftSpeed);
     setMotorSpeed(RIGHT, rightSpeed);
-  }
+  }*/
 #else
   #error A motor driver must be selected!
 #endif

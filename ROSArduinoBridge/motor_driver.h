7d68e3fef7245d283da1b3000dec0149867f064d
Althahir10 <127445963+Althahir10@users.noreply.github.com>
2024-04-17 16:26:16 -0500

    Update motor_driver.h

--- ROSArduinoBridge/motor_driver.h
+++ ROSArduinoBridge/motor_driver.h
@@ -8,15 +8,12 @@
 #ifdef Spark_Motor_Controller
 #include <Servo.h>
 
-#define Rservopin 12
-#define Lservopin 13
-
 Servo RIGHT_MOTOR_PWM; // Declare servo object for right motor
 Servo LEFT_MOTOR_PWM;  // Declare servo object for left motor
 
 void initMotors() {
-  RIGHT_MOTOR_PWM.attach(Rservopin); // Attach right servo to pin 12
-  LEFT_MOTOR_PWM.attach(Lservopin);  // Attach left servo to pin 13
+  RIGHT_MOTOR_PWM.attach(12); // Attach right servo to pin 12
+  LEFT_MOTOR_PWM.attach(13);  // Attach left servo to pin 13
 }
 
 

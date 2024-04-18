95feec7086a7b5b2563e4f2254cc7956356e6a0a
Althahir10 <127445963+Althahir10@users.noreply.github.com>
2024-04-18 03:11:38 -0500

    Update motor_driver.h

--- ROSArduinoBridge/motor_driver.h
+++ ROSArduinoBridge/motor_driver.h
@@ -2,38 +2,27 @@
    Motor driver function definitions - by James Nugen
    *************************************************************/
 
-
-
-
 #ifdef Spark_Motor_Controller
 #include <Servo.h>
 
 Servo RIGHT_MOTOR_PWM; // Declare servo object for right motor
 Servo LEFT_MOTOR_PWM;  // Declare servo object for left motor
 
-void initMotors() {
-  RIGHT_MOTOR_PWM.attach(11); // Attach right servo to pin 11
-  LEFT_MOTOR_PWM.attach(13);  // Attach left servo to pin 13
-}
+//void initMotors() {
+  //RIGHT_MOTOR_PWM.attach(11); // Attach right servo to pin 11
+  //LEFT_MOTOR_PWM.attach(10);  // Attach left servo to pin 10
+
+#define  RIGHT_MOTOR_PIN 11 // Attach right servo to pin 11
+#define  LEFT_MOTOR_PIN  10  // Attach left servo to pin 10
+//}
 
 
 #endif
-//void initMotorController();
+void initMotorController();
 void setMotorSpeed(int i, int spd);
 void setMotorSpeeds(int leftSpeed, int rightSpeed);
 
 
 
-/*#define RIGHT_MOTOR_BACKWARD 5
-  #define LEFT_MOTOR_BACKWARD  6
-  #define RIGHT_MOTOR_FORWARD  9
-  #define LEFT_MOTOR_FORWARD   10
-  #define RIGHT_MOTOR_ENABLE 12
-  #define LEFT_MOTOR_ENABLE 13
-#endif
-
-void initMotorController();
-void setMotorSpeed(int i, int spd);
-void setMotorSpeeds(int leftSpeed, int rightSpeed);*/
 
 

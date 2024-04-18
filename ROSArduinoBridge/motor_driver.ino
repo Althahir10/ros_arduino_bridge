df59d6e103833c2f4c61e072082a1459d2573791
Althahir10 <127445963+Althahir10@users.noreply.github.com>
2024-04-18 02:05:37 -0500

    Update motor_driver.ino

--- ROSArduinoBridge/motor_driver.ino
+++ ROSArduinoBridge/motor_driver.ino
@@ -1,18 +1,80 @@
 
 ​
-​
-#ifdef USE_BASE
+​#ifdef USE_BASE
    
 #ifdef POLOLU_VNH5019
 #elif defined Spark_Motor_Controller
 
   void setMotorSpeed(int i, int spd) {
-    if (i == LEFT) RIGHT_MOTOR_PWM.write(spd );
-    else LEFT_MOTOR_PWM.write(spd );  }
+    unsigned char reverse = 0;
+  
+    // Reverse direction if speed is negative
+    if (spd < 0) {
+        spd = -spd;
+        reverse = 1;
+    }
+    if (spd > 255)
+        spd = 255;
+    
+    // Set speed based on direction
+    if (i == LEFT) {
+        if (reverse == 0) {
+            LEFT_MOTOR_PWM.writeMicroseconds(1500 + spd); // Center position + speed
+        } else if (reverse == 1) {
+            LEFT_MOTOR_PWM.writeMicroseconds(1500 - spd); // Center position - speed
+        }
+    } else {
+        if (reverse == 0) {
+            RIGHT_MOTOR_PWM.writeMicroseconds(1500 + spd); // Center position + speed
+        } else if (reverse == 1) {
+            RIGHT_MOTOR_PWM.writeMicroseconds(1500 - spd); // Center position - speed
+        }
+    }
+}
 
   void setMotorSpeeds(int leftSpeed, int rightSpeed) {
     setMotorSpeed(LEFT, leftSpeed);
     setMotorSpeed(RIGHT, rightSpeed);                 }
+
+
+
+
+
+/*  void initMotorController() {
+    digitalWrite(RIGHT_MOTOR, HIGH);
+    digitalWrite(LEFT_MOTOR, HIGH);
+  }
+ 
+  void setMotorSpeed(int i, int spd) {
+    unsigned char reverse = 0;
+ 
+    if (spd < 0)
+    {
+      spd = -spd;
+      reverse = 1;
+    }
+    if (spd > 255)
+      spd = 255;
+   
+    if (i == LEFT) {
+      if      (reverse == 0) { LEFT_MOTOR_PWM.write(spd ); }
+      else if (reverse == 1) { LEFT_MOTOR_PWM.write(spd ); }
+    }
+    else /*if (i == RIGHT) //no need for condition {
+      if      (reverse == 0) { RIGHT_MOTOR_PWM.write(spd );}
+      else if (reverse == 1) { RIGHT_MOTOR_PWM.write(spd );}
+    }
+  }
+ 
+  void setMotorSpeeds(int leftSpeed, int rightSpeed) {
+    setMotorSpeed(LEFT, leftSpeed);
+    setMotorSpeed(RIGHT, rightSpeed);
+  }*/
+#else
+  #error A motor driver must be selected!
+#endif
+
+#endif
 
 
 

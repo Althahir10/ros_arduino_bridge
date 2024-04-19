554235ae3a5c456968ceef670cc67d49452166fe
Althahir10 <127445963+Althahir10@users.noreply.github.com>
2024-04-19 11:35:13 -0500

    Update motor_driver8.h

--- ROSArduinoBridge/motor_driver.h
+++ ROSArduinoBridge/motor_driver.h
@@ -1,23 +1,15 @@
 /***************************************************************
    Motor driver function definitions - by James Nugen
    *************************************************************/
-
 #ifdef Spark_Motor_Controller
 #include <Servo.h>
 
-Servo RIGHT_MOTOR_PWM; // Declare servo object for right motor
-Servo LEFT_MOTOR_PWM;  // Declare servo object for left motor
-
-//void initMotors() {
-  //RIGHT_MOTOR_PWM.attach(11); // Attach right servo to pin 11
-  //LEFT_MOTOR_PWM.attach(10);  // Attach left servo to pin 10
-
-#define  RIGHT_MOTOR_PIN 11 // Attach right servo to pin 11
-#define  LEFT_MOTOR_PIN  10  // Attach left servo to pin 10
-//}
-
+// Define servo objects for left and right motors
+extern Servo leftMotor;
+extern Servo rightMotor;
 
 #endif
+
 void initMotorController();
 void setMotorSpeed(int i, int spd);
 void setMotorSpeeds(int leftSpeed, int rightSpeed);

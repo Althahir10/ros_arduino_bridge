739a6db92ce66ed80738a4a6b4d3e6cfaa319698
Althahir10 <127445963+Althahir10@users.noreply.github.com>
2024-04-22 18:43:37 -0500

    Update motor_driver.h

--- ROSArduinoBridge/motor_driver.h
+++ ROSArduinoBridge/motor_driver.h
@@ -2,7 +2,8 @@
    Motor driver function definitions - by James Nugen
    *************************************************************/
 #ifdef Spark_Motor_Controller
-
+#include <Servo.h>
+Servo leftMotor, rightMotor;
   #define RIGHT_MOTOR_PIN 9
   #define LEFT_MOTOR_PIN 10
 

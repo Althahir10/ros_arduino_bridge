4de575b65c5d1f110706d33c5e872688ced987c8
Althahir10 <127445963+Althahir10@users.noreply.github.com>
2024-04-22 13:08:21 -0500

    Update motor_driver.ino

--- ROSArduinoBridge/motor_driver.ino
+++ ROSArduinoBridge/motor_driver.ino
@@ -1,49 +1,32 @@
+
 #ifdef USE_BASE
    
 #ifdef POLOLU_VNH5019
 #elif defined Spark_Motor_Controller
 
 
-  void initMotorController() {
-    pinMode(RIGHT_MOTOR_PIN, OUTPUT);
-    pinMode(LEFT_MOTOR_PIN, OUTPUT);
-  }
+#include <Servo.h>
 
+Servo leftMotor, rightMotor;
+void initMotorController(){
+  leftMotor.attach(LEFT_MOTOR_PIN);
+  rightMotor.attach(RIGHT_MOTOR_PIN);
+                  }
 
-      
 void setMotorSpeed(int i, int spd) {
-  unsigned char reverse = 0;
-  
-  if (spd < 0) {
-    spd = -spd;  // Make spd positive
-    reverse = 1; // Set reverse to true
-  }
-  
-  if (spd > 255) {
-    spd = 255;   // Limit spd to 255
-  }
-  
-  // Map spd to the range expected by the motor controller
-  if (reverse) {
-    spd = map(spd, 0, 255, 0, 90); // Map to 0-127 for reverse
-  } else {
-    spd = map(spd, 0, 255, 95, 180); // Map to 128-255 for forward
-  }
-  
-  // Write the mapped spd value to the motor
+  // Map spd from -255-255 to 1000-2000
+  spd = map(spd, -255, 255, 1000, 2000);
+
   if (i == LEFT) {
-    analogWrite(LEFT_MOTOR_PIN, spd);
+    leftMotor.writeMicroseconds(spd);
   } else /*if (i == RIGHT)*/ {
-    analogWrite(RIGHT_MOTOR_PIN, spd);
+    rightMotor.writeMicroseconds(spd);
   }
 }
-
-  
-  void setMotorSpeeds(int leftSpeed, int rightSpeed) {
-    setMotorSpeed(LEFT, leftSpeed);
-    setMotorSpeed(RIGHT, rightSpeed);
-  }
-
+void setMotorSpeeds(int leftSpeed, int rightSpeed) {
+  setMotorSpeed(LEFT, leftSpeed);
+  setMotorSpeed(RIGHT, rightSpeed);
+                                                    }
 #else
   #error A motor driver must be selected!
 #endif

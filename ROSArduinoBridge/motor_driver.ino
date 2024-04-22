022e8d5ccf939d39c18c4ffb7d07a841b4ecb256
Althahir10 <127445963+Althahir10@users.noreply.github.com>
2024-04-22 16:46:01 -0500

    Update motor_driver.ino

--- ROSArduinoBridge/motor_driver.ino
+++ ROSArduinoBridge/motor_driver.ino
@@ -14,14 +14,14 @@
       
   void setMotorSpeed(int i, int spd) {
       unsigned char reverse = 0;
-           // Map spd from -255-255 to 1000-2000
-      spd = map(spd, -255, 255, 1000, 2000);
       if (spd < 0)
     {
-
       spd = -spd;
       reverse = 1;
     }
+    if (spd > 255)
+      spd = 255;
+
       
     if (i == LEFT) {
       if      (reverse == 0){analogWrite(LEFT_MOTOR_PIN, spd);}

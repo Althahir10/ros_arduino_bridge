b3e81fa5425d337ee404228b58f32b3a80d2d48a
Althahir10 <127445963+Althahir10@users.noreply.github.com>
2024-04-22 16:19:11 -0500

    Update motor_driver.ino

--- ROSArduinoBridge/motor_driver.ino
+++ ROSArduinoBridge/motor_driver.ino
@@ -5,28 +5,43 @@
 #elif defined Spark_Motor_Controller
 
 
-#include <Servo.h>
-
-Servo leftMotor, rightMotor;
-void initMotorController(){
-  leftMotor.attach(LEFT_MOTOR_PIN);
-  rightMotor.attach(RIGHT_MOTOR_PIN);
-                  }
-
-void setMotorSpeed(int i, int spd) {
-  // Map spd from -255-255 to 1000-2000
-  spd = map(spd, -255, 255, 1000, 2000);
-
-  if (i == LEFT) {
-    leftMotor.write(spd);
-  } else /*if (i == RIGHT)*/ {
-    rightMotor.write(spd);
+  void initMotorController() {
+    pinMode(RIGHT_MOTOR_PIN, OUTPUT);
+    pinMode(LEFT_MOTOR_PIN, OUTPUT);
   }
-}
-void setMotorSpeeds(int leftSpeed, int rightSpeed) {
-  setMotorSpeed(LEFT, leftSpeed);
-  setMotorSpeed(RIGHT, rightSpeed);
-                                                    }
+
+
+      
+  void setMotorSpeed(int i, int spd) {
+      unsigned char reverse = 0;
+           // Map spd from -255-255 to 1000-2000
+      spd = map(spd, -255, 255, 1000, 2000);
+      if (spd < 0)
+    {
+
+      spd = -spd;
+      reverse = 1;
+    }
+    if (spd > 255)
+      spd = 255;
+
+      
+    if (i == LEFT) {
+      if      (reverse == 0){analogWrite(LEFT_MOTOR_PIN, spd);}
+      else if (reverse == 1){analogWrite(LEFT_MOTOR_PIN, -spd);}
+      
+    }
+    else /*if (i == RIGHT)*/ {
+      if      (reverse == 0){analogWrite(RIGHT_MOTOR_PIN, spd);}
+      if      (reverse == 1){analogWrite(RIGHT_MOTOR_PIN, -spd);}
+    }
+  }
+  
+  void setMotorSpeeds(int leftSpeed, int rightSpeed) {
+    setMotorSpeed(LEFT, leftSpeed);
+    setMotorSpeed(RIGHT, rightSpeed);
+  }
+
 #else
   #error A motor driver must be selected!
 #endif

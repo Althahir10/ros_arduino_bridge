118647cc51b3eea434c6a0db854f1d165cd961ce
Althahir10 <127445963+Althahir10@users.noreply.github.com>
2024-04-25 18:52:30 -0500

    Update motor_driver.ino

--- ROSArduinoBridge/motor_driver.ino
+++ ROSArduinoBridge/motor_driver.ino
@@ -1,34 +1,33 @@
-
 #ifdef USE_BASE
 #ifdef POLOLU_VNH5019
 #elif defined Spark_Motor_Controller //Spark motor macro
 
+#include <Servo.h>
+
+//Servo leftMotor, rightMotor;
 
 void initMotorController() {
   leftMotor.attach(LEFT_MOTOR_PIN);
   rightMotor.attach(RIGHT_MOTOR_PIN);
-
 }
 
-
 void setMotorSpeed(int i, int spd) {
+  int pwm;
 
-    if (spd < 0)
-    {
-      spd = -spd;
-//      reverse = 1;
-    }
-    if (spd > 255)
-      spd = 255;
+  if (spd >= 0) {
+    // Map forward speed to appropriate PWM value
+    pwm = map(spd, 0, 255, 1535, 2000); // Neutral to full forward
+  } else {
+    // Map reverse speed to appropriate PWM value
+    pwm = map(-spd, 0, 255, 1465, 1000); // Full reverse to neutral
+  }
 
-
-if (i == LEFT) {
-    leftMotor.writeMicroseconds(spd);
+  if (i == LEFT) {
+    leftMotor.writeMicroseconds(pwm);
   } else /*if (i == RIGHT)*/ {
-    rightMotor.writeMicroseconds(spd);
+    rightMotor.writeMicroseconds(pwm);
   }
-  }
-
+}
 
 void setMotorSpeeds(int leftSpeed, int rightSpeed) {
   setMotorSpeed(LEFT, leftSpeed);
@@ -39,4 +38,4 @@ void setMotorSpeeds(int leftSpeed, int rightSpeed) {
 #error A motor driver must be selected!
 #endif
 
-#endif
+#endif 

d49447a3b136336e973510f0744eb934f3ed36ff
Althahir10 <127445963+Althahir10@users.noreply.github.com>
2024-04-25 18:42:17 -0500

    Update motor_driver.ino

--- ROSArduinoBridge/motor_driver.ino
+++ ROSArduinoBridge/motor_driver.ino
@@ -1,3 +1,4 @@
+
 #ifdef USE_BASE
 #ifdef POLOLU_VNH5019
 #elif defined Spark_Motor_Controller //Spark motor macro
@@ -11,22 +12,23 @@ void initMotorController() {
 
 
 void setMotorSpeed(int i, int spd) {
-  int pwm;
 
-  if (spd >= 0) {
-    // Map forward speed to appropriate PWM value
-    pwm = map(spd, 0, 255, 1535, 2000); // Neutral to full forward
-  } else {
-    // Map reverse speed to appropriate PWM value
-    pwm = map(-spd, 0, 255, 1465, 1000); // Full reverse to neutral
-  }
+    if (spd < 0)
+    {
+      spd = -spd;
+//      reverse = 1;
+    }
+    if (spd > 255)
+      spd = 255;
 
-  if (i == LEFT) {
-    analogWrite(LEFT_MOTOR_PIN, pwm);
+
+if (i == LEFT) {
+    leftMotor.writeMicroseconds(spd);
   } else /*if (i == RIGHT)*/ {
-    analogWrite(RIGHT_MOTOR_PIN, pwm);
+    rightMotor.writeMicroseconds(spd);
   }
-}
+  }
+
 
 void setMotorSpeeds(int leftSpeed, int rightSpeed) {
   setMotorSpeed(LEFT, leftSpeed);

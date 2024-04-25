e9a525b1eb44063546760be1075d867691de56db
Althahir10 <127445963+Althahir10@users.noreply.github.com>
2024-04-25 17:10:36 -0500

    Update motor_driver.ino

--- ROSArduinoBridge/motor_driver.ino
+++ ROSArduinoBridge/motor_driver.ino
@@ -1,43 +1,30 @@
 #ifdef USE_BASE
-   
 #ifdef POLOLU_VNH5019
-#elif defined Spark_Motor_Controller
-
-   
+#elif defined Spark_Motor_Controller //Spark motor macro
 void setMotorSpeed(int i, int spd) {
   int reverse = 0;
-
   if (spd < 0) {
     spd = -spd;  // Make speed positive
     reverse = 1; // Note the direction
   }
-  if (spd > 255)
-      spd = 255;
-      
+  if (spd > 255) {
+    spd = 255;   }
   // Map speed to appropriate PWM value for Spark Motor Controller
   if (reverse) {
-    spd = map(spd, 0, 255, 1000, 1400); // Full reverse to neutral
+    spd = map(spd, 0, 255, 1000, 1465); // Full reverse to neutral
   } else {
-    spd = map(spd, 0, 255, 1560, 2000); // Neutral to full forward
-  }
+    spd = map(spd, 0, 255, 1535, 2000); // Neutral to full forward
+  }  //deadband range changed from 1460-1540 to 1465-1535
 
   if (i == LEFT) {
     analogWrite(LEFT_MOTOR_PIN, spd);
-  } else /*if (i == RIGHT)*/ {
-    analogWrite(RIGHT_MOTOR_PIN, spd);
-  }
+  } else { /*if (i == RIGHT)*/
+    analogWrite(RIGHT_MOTOR_PIN, spd);  }
 }
-
-
-
-  
-  void setMotorSpeeds(int leftSpeed, int rightSpeed) {
-    setMotorSpeed(LEFT, leftSpeed);
-    setMotorSpeed(RIGHT, rightSpeed);
-  }
-
+void setMotorSpeeds(int leftSpeed, int rightSpeed) {
+  setMotorSpeed(LEFT, leftSpeed);
+  setMotorSpeed(RIGHT, rightSpeed);                 }
 #else
-  #error A motor driver must be selected!
+#error A motor driver must be selected!
 #endif
-
 #endif

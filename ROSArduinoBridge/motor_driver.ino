389d28c96387213a198c76a93c91027e67b09d88
Althahir10 <127445963+Althahir10@users.noreply.github.com>
2024-04-25 21:12:56 -0500

    Update motor_driver.ino

--- ROSArduinoBridge/motor_driver.ino
+++ ROSArduinoBridge/motor_driver.ino
@@ -1,45 +1,45 @@
-
 #ifdef USE_BASE
+   
 #ifdef POLOLU_VNH5019
-#elif defined Spark_Motor_Controller //Spark motor macro
-void initMotorController() {
-  leftMotor.attach(LEFT_MOTOR_PIN);
-  rightMotor.attach(RIGHT_MOTOR_PIN);
-}
+#elif defined Spark_Motor_Controller
 
 
-void setMotorSpeed(int i, int spd) {
-  if (spd < 0)
+  void initMotorController() {
+    pinMode(RIGHT_MOTOR_PIN, OUTPUT);
+    pinMode(LEFT_MOTOR_PIN, OUTPUT);
+  }
+
+
+      
+  void setMotorSpeed(int i, int spd) {
+      unsigned char reverse = 0;
+      if (spd < 0)
     {
       spd = -spd;
-      //reverse = 1;
+      reverse = 1;
     }
     if (spd > 255)
       spd = 255;
-  int pwm;
 
-  if (spd >= 0) {
-    // Map forward speed to appropriate PWM value
-    pwm = map(spd, 0, 255, 1535, 2000); // Neutral to full forward
-  } else {
-    // Map reverse speed to appropriate PWM value
-    pwm = map(-spd, 0, 255, 1465, 1000); // Full reverse to neutral
+      
+    if (i == LEFT) {
+      if      (reverse == 0){analogWrite(LEFT_MOTOR_PIN, spd);}
+      else if (reverse == 1){analogWrite(LEFT_MOTOR_PIN, -spd);}
+      
+    }
+    else /*if (i == RIGHT)*/ {
+      if      (reverse == 0){analogWrite(RIGHT_MOTOR_PIN, spd);}
+      if      (reverse == 1){analogWrite(RIGHT_MOTOR_PIN, -spd);}
+    }
+  }
+  
+  void setMotorSpeeds(int leftSpeed, int rightSpeed) {
+    setMotorSpeed(LEFT, leftSpeed);
+    setMotorSpeed(RIGHT, rightSpeed);
   }
 
-  if (i == LEFT) {
-    //analogWrite(leftMotor, pwm);
-    leftMotor.writeMicroseconds(spd);
-  } else /*if (i == RIGHT)*/ {
-    //analogWrite(rightMotor, pwm);
-    rightMotor.writeMicroseconds(spd);
-  }
-}
-
-void setMotorSpeeds(int leftSpeed, int rightSpeed) {
-  setMotorSpeed(LEFT, leftSpeed);
-  setMotorSpeed(RIGHT, rightSpeed);
-}
 #else
-#error A motor driver must be selected!
+  #error A motor driver must be selected!
 #endif
+
 #endif

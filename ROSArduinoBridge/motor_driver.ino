46d6b5b3db69e0bd85bbab1e4013a6406ce2ed1f
Althahir10 <127445963+Althahir10@users.noreply.github.com>
2024-04-25 21:10:43 -0500

    Update motor_driver.ino

--- ROSArduinoBridge/motor_driver.ino
+++ ROSArduinoBridge/motor_driver.ino
@@ -1,43 +1,45 @@
+
 #ifdef USE_BASE
-   
 #ifdef POLOLU_VNH5019
-#elif defined Spark_Motor_Controller
-
-   
-void setMotorSpeed(int i, int spd) {
-  int reverse = 0;
-
-  if (spd < 0) {
-    spd = -spd;  // Make speed positive
-    reverse = 1; // Note the direction
-  }
-  if (spd > 255)
-      spd = 100;
-      
-  // Map speed to appropriate PWM value for Spark Motor Controller
-  if (reverse) {
-    spd = map(spd, 0, 255, 1000, 1465); // Full reverse to neutral
-  } else {
-    spd = map(spd, 0, 255, 1535, 2000); // Neutral to full forward
-  }
-
-  if (i == LEFT) {
-    analogWrite(LEFT_MOTOR_PIN, spd);
-  } else /*if (i == RIGHT)*/ {
-    analogWrite(RIGHT_MOTOR_PIN, spd);
-  }
+#elif defined Spark_Motor_Controller //Spark motor macro
+void initMotorController() {
+  leftMotor.attach(LEFT_MOTOR_PIN);
+  rightMotor.attach(RIGHT_MOTOR_PIN);
 }
 
 
+void setMotorSpeed(int i, int spd) {
+  if (spd < 0)
+    {
+      spd = -spd;
+      //reverse = 1;
+    }
+    if (spd > 255)
+      spd = 255;
+  int pwm;
 
-  
-  void setMotorSpeeds(int leftSpeed, int rightSpeed) {
-    setMotorSpeed(LEFT, leftSpeed);
-    setMotorSpeed(RIGHT, rightSpeed);
+  if (spd >= 0) {
+    // Map forward speed to appropriate PWM value
+    pwm = map(spd, 0, 255, 1535, 2000); // Neutral to full forward
+  } else {
+    // Map reverse speed to appropriate PWM value
+    pwm = map(-spd, 0, 255, 1465, 1000); // Full reverse to neutral
   }
 
-#else
-  #error A motor driver must be selected!
-#endif
+  if (i == LEFT) {
+    //analogWrite(leftMotor, pwm);
+    leftMotor.writeMicroseconds(spd);
+  } else /*if (i == RIGHT)*/ {
+    //analogWrite(rightMotor, pwm);
+    rightMotor.writeMicroseconds(spd);
+  }
+}
 
+void setMotorSpeeds(int leftSpeed, int rightSpeed) {
+  setMotorSpeed(LEFT, leftSpeed);
+  setMotorSpeed(RIGHT, rightSpeed);
+}
+#else
+#error A motor driver must be selected!
+#endif
 #endif
